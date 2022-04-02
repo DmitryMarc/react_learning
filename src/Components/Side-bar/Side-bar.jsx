18dc20464b26333ebde5743f42e39633a0af8283
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2022-04-02 16:27:18 +0300

    Create social network. Part 8. Using NavLink.

--- src/Components/Side-bar/Side-bar.jsx
+++ src/Components/Side-bar/Side-bar.jsx
@@ -1,14 +1,15 @@
+import { NavLink } from 'react-router-dom';
 import classes from  './Side-bar.module.css';
 
 const SideBar = () => {
     return (
         <nav className={classes.side__bar}>
             <ul>
-                <li className={classes.item}><a className={classes.active} href='/profile'>Ptofile</a></li>
-                <li className={classes.item}><a href='/dialogs'>Messages</a></li>
-                <li className={classes.item}><a href='/news'>News</a></li>
-                <li className={classes.item}><a href='/music'>Music</a></li>
-                <li className={classes.item}><a href='/settings'>Settings</a></li>
+                <li className={classes.item}><NavLink to='/profile' className = {navData => navData.isActive ? classes.activeLink : ''}>Ptofile</NavLink></li>
+                <li className={classes.item}><NavLink to='/dialogs' className = {navData => navData.isActive ? classes.activeLink : ''}>Messages</NavLink></li>
+                <li className={classes.item}><NavLink to='/news' className = {navData => navData.isActive ? classes.activeLink : ''}>News</NavLink></li>
+                <li className={classes.item}><NavLink to='/music' className = {navData => navData.isActive ? classes.activeLink : ''}>Music</NavLink></li>
+                <li className={classes.item}><NavLink to='/settings' className = {navData => navData.isActive ? classes.activeLink : ''}>Settings</NavLink></li>
             </ul>
         </nav>
     );
