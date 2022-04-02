e2649f315479d803faa0e7ec656568e457d149e0
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2022-04-02 23:09:32 +0300

    Create social network. Part 9. Improving component of Dialogs.

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import classes from './Dialogs.module.css'
 
 const Dialogs =  (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dmitry
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Sasha
+                </div>
+                <div className={classes.dialog}>
+                    Victor
+                </div>
+                <div className={classes.dialog}>
+                    Valery
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.dialog}>Hi</div>
+                <div className={classes.dialog}>How is your learning?</div>
+                <div className={classes.dialog}>React is the best!</div>
+            </div>
         </div>
     );
 }
--- src/Components/Side-bar/Side-bar.jsx
+++ src/Components/Side-bar/Side-bar.jsx
@@ -1,15 +1,17 @@
 import { NavLink } from 'react-router-dom';
 import classes from  './Side-bar.module.css';
 
+const setActive = (newData) => newData.isActive ? classes.activeLink : '';
+
 const SideBar = () => {
     return (
         <nav className={classes.side__bar}>
             <ul>
-                <li className={classes.item}><NavLink to='/profile' className = {navData => navData.isActive ? classes.activeLink : ''}>Ptofile</NavLink></li>
-                <li className={classes.item}><NavLink to='/dialogs' className = {navData => navData.isActive ? classes.activeLink : ''}>Messages</NavLink></li>
-                <li className={classes.item}><NavLink to='/news' className = {navData => navData.isActive ? classes.activeLink : ''}>News</NavLink></li>
-                <li className={classes.item}><NavLink to='/music' className = {navData => navData.isActive ? classes.activeLink : ''}>Music</NavLink></li>
-                <li className={classes.item}><NavLink to='/settings' className = {navData => navData.isActive ? classes.activeLink : ''}>Settings</NavLink></li>
+                <li className={classes.item}><NavLink to='/profile' className = {setActive}>Ptofile</NavLink></li>
+                <li className={classes.item}><NavLink to='/dialogs' className = {setActive}>Messages</NavLink></li>
+                <li className={classes.item}><NavLink to='/news' className = {setActive}>News</NavLink></li>
+                <li className={classes.item}><NavLink to='/music' className = {setActive}>Music</NavLink></li>
+                <li className={classes.item}><NavLink to='/settings' className = {setActive}>Settings</NavLink></li>
             </ul>
         </nav>
     );
