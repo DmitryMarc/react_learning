59d04db881dbcb815c648e3ceb5f1ade4644aaa3
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2022-04-03 15:55:17 +0300

    Create social network. Part 11. Moving data to an arrays of objects.

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -15,21 +15,41 @@ const Message = (props) => {
     );
 }
 
+
 const Dialogs =  (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dmitry'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Victor'},
+        {id: 6, name: 'Valery'}
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your learning?'},
+        {id: 3, message: 'React is the best!'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'},
+        {id: 6, message: 'Yo'}
+    ];
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name='Dmitry' id='1' />
-                <DialogItem name='Andrey' id='2' />
-                <DialogItem name='Sveta' id='3' />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                {/* <DialogItem name='Sveta' id='3' />
                 <DialogItem name='Sasha' id='4' />
                 <DialogItem name='Victor' id='5' />
-                <DialogItem name='Valery' id='6' />
+                <DialogItem name='Valery' id='6' /> */}
             </div>
             <div className={classes.messages}>
-                <Message message='Hi' />
-                <Message message='How is your learning?' />
-                <Message message='React is the best!' />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                {/* <Message message='React is the best!' /> */}
             </div>
         </div>
     );
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postsData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 15},
+        {id: 2, message: 'It\'s my first post', likesCount: 20}
+    ];
+
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
@@ -14,8 +20,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message='Hi, how are you?' likesCount='15' />
-                <Post message="It's my first post" likesCount='20' />
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
             </div>
         </div>
     );
