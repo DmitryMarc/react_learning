e87aa300c90877c8a0f36b8082894db215a03d37
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2022-04-07 23:24:54 +0300

    Create social network. Part 19. Refactoring files state.js and index.js. Packing of state and functions for to work with state into store.

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,58 +1,58 @@
-let rerenderEntireTree = () =>{
-    console.log('State chaged');
-}
-
-let state = {
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Dmitry' },
-            { id: 2, name: 'Andrey' },
-            { id: 3, name: 'Sveta' },
-            { id: 4, name: 'Sasha' },
-            { id: 5, name: 'Victor' },
-            { id: 6, name: 'Valery' }
-        ],
-        messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'How is your learning?' },
-            { id: 3, message: 'React is the best!' },
-            { id: 4, message: 'Yo' },
-            { id: 5, message: 'Yo' },
-            { id: 6, message: 'Yo' }
-        ]
+let store = {
+    _state: {
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Dmitry' },
+                { id: 2, name: 'Andrey' },
+                { id: 3, name: 'Sveta' },
+                { id: 4, name: 'Sasha' },
+                { id: 5, name: 'Victor' },
+                { id: 6, name: 'Valery' }
+            ],
+            messages: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'How is your learning?' },
+                { id: 3, message: 'React is the best!' },
+                { id: 4, message: 'Yo' },
+                { id: 5, message: 'Yo' },
+                { id: 6, message: 'Yo' }
+            ]
+        },
+        profilePage:  {
+            postsData: [
+                { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+                { id: 2, message: 'It\'s my first post', likesCount: 20 },
+                { id: 2, message: 'It\'s my second post', likesCount: 21 },
+                { id: 2, message: 'It\'s my coolest post', likesCount: 4 }
+            ],
+            newPostText: 'Dmitry.com'
+        }
     },
-    profilePage:  {
-        postsData: [
-            { id: 1, message: 'Hi, how are you?', likesCount: 15 },
-            { id: 2, message: 'It\'s my first post', likesCount: 20 },
-            { id: 2, message: 'It\'s my second post', likesCount: 21 },
-            { id: 2, message: 'It\'s my coolest post', likesCount: 4 }
-        ],
-        newPostText: 'Dmitry.com'
+    getState(){
+        return this._state;
+    },
+    _callSubscriber(){
+        console.log('State chaged');
+    },
+    addPost(){
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.postsData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText){
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer){
+        this._callSubscriber = observer; // pattern, наблюдатель
     }
 }
 
-window.state = state;
+export default store;
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    }
-    state.profilePage.postsData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export let subscribe = (observer) => {
-    rerenderEntireTree = observer; // pattern, наблюдатель
-}
-
-
-export default state;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './Redux/state';
+import store from './Redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost, updateNewPostText } from './Redux/state';
 
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+                <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
@@ -20,9 +19,9 @@ let rerenderEntireTree = (state) => {
 }
 
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
