ccc7ef9303fafd4549dad0b8614f38f5ccddf6d3
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2022-04-23 18:03:03 +0300

    Create social network. Part 34. Using componentDidMount method.

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -5,13 +5,10 @@ import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-        alert('NEW');
+    componentDidMount(){
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items);
         });
-
     }
 
 
