04c067f7c84da29abf53059ff33689c528683029
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2022-10-20 17:51:44 +0300

    Create social network. Part 64. Created tdd unit tests for profileReducer.

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -2,13 +2,14 @@ import { profileAPI, usersAPI } from "../api/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
 
 let initialState = {
     postsData: [
         { id: 1, message: 'Hi, how are you?', likesCount: 15 },
         { id: 2, message: 'It\'s my first post', likesCount: 20 },
-        { id: 2, message: 'It\'s my second post', likesCount: 21 },
-        { id: 2, message: 'It\'s my coolest post', likesCount: 4 }
+        { id: 3, message: 'It\'s my second post', likesCount: 21 },
+        { id: 4, message: 'It\'s my coolest post', likesCount: 4 }
     ],
     profile: null,
     status: ""
@@ -41,6 +42,12 @@ const profileReducer = (state = initialState, action) => {
                 profile: action.profile
             };
         }
+        case DELETE_POST: {
+            return {
+                ...state,
+                postsData: state.postsData.filter(post => post.id != action.postId)
+            }
+        }
         default:
             return state;
     }
@@ -51,6 +58,8 @@ export const addPostActionCreator = (newPostText) => ({ type: ADD_POST, newPostT
 export const setUserProfileActionCreator = (profile) => ({ type: SET_USER_PROFILE, profile });
 
 export const setStatusActionCreator = (status) => ({ type: SET_STATUS, status });
+
+export const deletePostActionCreator = (postId) => ({ type: DELETE_POST, postId });
 
 export const getUserProfileThunkCreator = (userId) => (dispatch) => (
     usersAPI.getProfile(userId).then(response => {
