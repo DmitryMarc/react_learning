0eb99f24e75a25d6bd4845994c35bf13dd2b00a7
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2023-01-05 23:52:39 +0300

    Create social network. Part 101. Code refactoring. Improved the design of the user's login form.

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -8,8 +8,10 @@ import { loginTC } from "../../Redux/auth-reducer";
 import { Redirect } from "react-router-dom";
 import style from "./../common/FormsControls/FormsControls.module.css";
 import { FC } from "react";
-import { AppDispatchType, AppStateType } from "../../Redux/redux-store";
-import { selectCaptchaUrl, selectIsAuth } from "../../Redux/selectors/auth-selectors";
+import { AppDispatchType } from "../../Redux/redux-store";
+import { selectCaptchaUrl, selectIsAuth }
+    from "../../Redux/selectors/auth-selectors";
+import { Button, Col, Row } from "antd";
 
 type LoginFormOwnProps = {
     captchaUrl: string | null | undefined
@@ -19,30 +21,61 @@ const LoginForm: FC<InjectedFormProps<LoginFormValuesType, LoginFormOwnProps> &
     LoginFormOwnProps> = ({ handleSubmit, error, captchaUrl }) => {
         return (
             <form onSubmit={handleSubmit}>
-                {createField<LoginFormValuesTypeKeys>
-                    ("Email", "email", [required], Input)}
-                {createField<LoginFormValuesTypeKeys>
-                    ("Password", "password", [required], Input, { type: "password" })}
-                {createField<LoginFormValuesTypeKeys>
-                    (undefined, "rememberMe", [], Input, { type: "checkbox" }, "rememder me")}
-
-                {captchaUrl && <img src={captchaUrl} />}
-                {captchaUrl && createField<LoginFormValuesTypeKeys>
-                    ("Symbols from image", "captcha", [required], Input, {})}
-
-                {error && <div className={style.formSummaryError}>
-                    {error}
-                </div>
-                }
-                <div>
-                    <button>Sign in</button>
-                </div>
+                {/* <Row>
+                    <Col span={24}>col</Col>
+                </Row> */}
+                <Row gutter={[0, 8]}>
+                    <Col span={4}>
+                        {createField<LoginFormValuesTypeKeys>
+                            ("Email", "email", [required], Input)}
+                    </Col>
+                    <Col span={20}>
+                    </Col>
+                    <Col span={4}>
+                        {createField<LoginFormValuesTypeKeys>
+                            ("Password", "password", [required], Input, { type: "password" })}
+                    </Col>
+                    <Col span={20}>
+                    </Col>
+                    <Col span={3}>
+                        <div style={{ display: "inline-block" }}>
+                            <span>rememder me: </span>
+                            <span style={{ display: "inline-block" }}>{createField<LoginFormValuesTypeKeys>
+                                (undefined, "rememberMe", [], Input, { type: "checkbox" }, "")}</span>
+                        </div>
+                    </Col>
+                    <Col span={1}>
+                        {/* <button>Sign in</button> */}
+                        <Button htmlType="submit" style={{ float: "right" }}>Sign in</Button>
+                    </Col>
+                    <Col span={20}>
+                    </Col>
+                    <Col span={4}>
+                        {captchaUrl && <img src={captchaUrl} />}
+                    </Col>
+                    <Col span={20}>
+                    </Col>
+                    <Col span={4}>
+                        {captchaUrl && createField<LoginFormValuesTypeKeys>
+                            ("Symbols from image", "captcha", [required], Input, {})}
+                    </Col>
+                    <Col span={20}>
+                    </Col>
+                    <Col span={4}>
+                        {error && <div className={style.formSummaryError}>
+                            {error}
+                        </div>
+                        }
+                    </Col>
+                    <Col span={20}>
+                    </Col>
+                </Row>
             </form>
         )
     }
 
-const LoginReduxForm = reduxForm<LoginFormValuesType,
-    LoginFormOwnProps>({ form: 'login' })(LoginForm);
+const LoginReduxForm = reduxForm<LoginFormValuesType, LoginFormOwnProps>(
+    { form: 'login' })(LoginForm);
 
 export type LoginFormValuesType = {
     email: string,
@@ -71,5 +104,3 @@ export const LoginPage: FC = () => {
         <LoginReduxForm onSubmit={onSubmit} captchaUrl={captchaUrl} />
     </div>
 }
-
-// Организовать красивую стулизацию формы логинизации!
